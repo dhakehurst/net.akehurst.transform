e4bacb118b1ebbcf44e63c45244bee2d3f7b1f7c
David Akehurst <dhakehurst@users.noreply.github.com>
2023-08-30 10:24:37 +0200

    kotlin version 1.9.10

--- net.akehurst.transform/build.gradle.kts
+++ net.akehurst.transform/build.gradle.kts
@@ -18,11 +18,11 @@ import com.github.gmazzo.gradle.plugins.BuildConfigExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 plugins {
-    kotlin("multiplatform") version ("1.9.0") apply false
-    id("org.jetbrains.dokka") version ("1.8.10") apply false
-    id("com.github.gmazzo.buildconfig") version ("3.1.0") apply false
+    kotlin("multiplatform") version ("1.9.10") apply false
+    id("org.jetbrains.dokka") version ("1.8.20") apply false
+    id("com.github.gmazzo.buildconfig") version ("4.1.2") apply false
     id("nu.studer.credentials") version ("3.0")
-    id("net.akehurst.kotlin.gradle.plugin.exportPublic") version ("1.9.0") apply false
+    id("net.akehurst.kotlin.gradle.plugin.exportPublic") version ("1.9.10") apply false
 }
 val kotlin_languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8
 val kotlin_apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_8
@@ -185,7 +185,4 @@ subprojects {
         sign(publishing.publications)
     }
 
-    rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-        rootProject.the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().version = "1.22.19"
-    }
 }
